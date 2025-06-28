aa24d0373b6b396d0c8c504137dc1f34b0b0f86f
emawid <140737336+emawid@users.noreply.github.com>
2025-06-28 16:01:22 +0200

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -13,8 +13,28 @@ function populateIcon(icon, title) {
 function themeToggle() {
   const toggle = document.querySelector('#theme-toggle');
 
+  //On page load, set theme from localStorage
+  const savedTheme = localStorage.getItem('theme');
+
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark');
+    toggle.checked = true;
+  } else {
+    document.body.classList.remove('dark');
+    toggle.checked = false;
+  }
+
+  //Listen for toggle changes
   toggle.addEventListener('change', () => {
     document.body.classList.toggle('dark', toggle.checked);
+
+    if (toggle.checked) {
+      document.body.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark');
+      localStorage.removeItem('dark');
+    }
   });
 }
 
