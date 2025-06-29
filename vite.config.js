6db30e20294ca9f287c302c5e7766d0009288fe4
emawid <140737336+emawid@users.noreply.github.com>
2025-06-29 18:34:47 +0200

    vite.config.js updated to explicitly include all html entrypoints

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
+import { resolve } from 'path';
+
 export default {
   // config options
 
   base: '/frontend-quiz-app/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        questions: resolve(__dirname, 'questions.html'),
+        score: resolve(__dirname, 'score.html'),
+      },
+    },
+  },
 };
